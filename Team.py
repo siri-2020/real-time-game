e43e1461d80a2480fa5eac18d3fa27601760c13d
siri-2020 <tanyakarn.siri2020@gmail.com>
2026-01-14 13:08:33 +0700

    commit

--- Team.py
+++ Team.py
@@ -2,4 +2,3 @@ print("Team name: moo ma ka kai 1 2 3 4")
 
 print("Tanyakarn")
 print("Taechasith")
-print("Tanyakarn Siri.")
--- game.py
+++ game.py
@@ -29,6 +29,9 @@ running = True
 player_x = 10
 player_y = 10
 
+npc_x = 90
+npc_y = 90
+
 x_min = 0
 x_max = 100
 y_min = 0
@@ -38,10 +41,22 @@ def scan_keys():
     return kb.pressed
 
 def render_state():
-    print("player is at:", player_x, player_y)
+    print(f"\rplayer position: ({player_x}, {player_y})\nnpc is at: ({npc_x}, {npc_y})", end="")
+    #print(f"\rnpc is at: ({npc_x}, {npc_y})", end="")
 
 def update_state(inp):
-    global player_x, player_y, running
+    global player_x, player_y, running, npc_x, npc_y
+    
+    x_walk = 1
+    y_walk = 2
+
+    if npc_x > x_max or npc_x < x_min:
+        x_walk = x_walk
+        x_walk = -x_walk
+    if npc_y > y_max or npc_y < y_min:
+        y_walk = y_walk
+        y_walk = -y_walk    
+
     if "a" in inp:
         player_x -= 1
     elif "d" in inp:
@@ -62,6 +77,11 @@ def update_state(inp):
     if player_y > y_max:
         player_y = y_max
 
+    # when npc reaches boundary, reset it bounce back in oposite direction
+ 
+    npc_x += x_walk
+    npc_y += y_walk
+
 while running:
     # read/check for user actions (input)
     # update game state (physics, AI, etc)
@@ -71,4 +91,4 @@ while running:
     inp = scan_keys()
 
     update_state(inp)
-    time.sleep(0.1)
+    time.sleep(0.5)
