9edd70060af54098435aa18806b2352db8a8c703
siri-2020 <tanyakarn.siri2020@gmail.com>
2026-01-15 11:20:50 +0700

    update to teacher's version

--- npc.py
+++ npc.py
@@ -1,15 +1,15 @@
 class NPC:
-    def __init__(self, x, y, speed_x, speed_y):
+    def __init__(self, x, y, speed_x=100, speed_y=100):
         self.x = x
         self.y = y
         self.speed_x = speed_x
         self.speed_y = speed_y
-
-    def update(self, bounds):
-        self.x += self.speed_x
-        self.y += self.speed_y
-
-        if self.x <= bounds["x_min"] or self.x >= bounds["x_max"]:
-            self.speed_x *= -1
-        if self.y <= bounds["y_min"] or self.y >= bounds["y_max"]:
-            self.speed_y *= -1
+    
+    def move(self, game_field, dt):
+        self.x += self.speed_x * dt
+        self.y += self.speed_y * dt
+        self.x, self.y, x_edge, y_edge = game_field.clamp(self.x, self.y)
+        if x_edge:
+            self.speed_x = -self.speed_x
+        if y_edge:
+            self.speed_y = -self.speed_y
--- player.py
+++ player.py
@@ -1,17 +1,11 @@
 class Player:
-    def __init__(self, x, y):
+    def __init__(self, x, y, speed_x=300, speed_y=300):
         self.x = x
         self.y = y
-
-    def update(self, keys, bounds):
-        if "a" in keys:
-            self.x -= 1
-        if "d" in keys:
-            self.x += 1
-        if "w" in keys:
-            self.y -= 1
-        if "s" in keys:
-            self.y += 1
-
-        self.x = max(bounds["x_min"], min(self.x, bounds["x_max"]))
-        self.y = max(bounds["y_min"], min(self.y, bounds["y_max"]))
+        self.speed_x = speed_x
+        self.speed_y = speed_y
+    
+    def move(self, left, right, up, down, game_field, dt):
+        self.x += self.speed_x * dt * right - self.speed_x * dt * left
+        self.y += self.speed_y * dt * down - self.speed_y * dt * up
+        self.x, self.y, _, _ = game_field.clamp(self.x, self.y)
